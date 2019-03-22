16f0d9a5bc4d0f7649949ea4bc9579c472bc8ece
Daniel Rodríguez Troitiño <daniel.rodriguez@oculus.com>
2019-03-22 10:47:00 -0700

    [build-script] CMark Python builder.
    CMark builder using the CMakeProductBuilder. This example shows how simple a builder for a product can be. It is just inheriting, defining/extending new CMake options, and filling up a couple of template methods (in this case _build_variant, _test_executable_target, and _test_results_targets).

--- utils/swift_build_support/swift_build_support/products/cmark.py
+++ utils/swift_build_support/swift_build_support/products/cmark.py
@@ -11,7 +11,48 @@
 # ----------------------------------------------------------------------------
 
 from . import product
+from .cmake_product_builder import CMakeProductBuilder
+from .. import shell
 
 
 class CMark(product.Product):
-    pass
+    @classmethod
+    def make_builder(cls, args, toolchain, workspace, host):
+        return CMarkBuilder(args, toolchain, workspace, host)
+
+
+class CMarkBuilder(CMakeProductBuilder):
+    def __init__(self, args, toolchain, workspace, host):
+        CMakeProductBuilder.__init__(
+            self, CMark, args, toolchain, workspace, host)
+
+        self._cmake_options.define(
+            'CMAKE_BUILD_TYPE:STRING', self._build_variant)
+
+        if host.platform.is_darwin:
+            cmark_c_flags = self._common_cross_cflags
+            if self._is_release_build_variant:
+                cmark_c_flags += ' -fno-stack-protector'
+            sdk_name = shell.xcrun_show_sdk_path(host.platform.name)
+            self._cmake_options.extend([
+                ('CMAKE_C_FLAGS', cmark_c_flags),
+                ('CMAKE_CXX_FLAGS', cmark_c_flags)
+                ('CMAKE_OSX_SYSROOT:PATH', sdk_name)])
+            if host.name == 'macosx-x86_64':
+                self._cmake_options.define('CMAKE_OSX_DEPLOYMENT_TARGET',
+                                           args.darwin_deployment_version_osx)
+
+    @property
+    def _build_variant(self):
+        return self._args.cmark_build_variant
+
+    @property
+    def _test_executable_target(self):
+        return 'api_test'
+
+    @property
+    def _test_results_targets(self):
+        if self._args.cmake_generator == 'Xcode':
+            return ['RUN_TESTS']
+        else:
+            return ['test']
